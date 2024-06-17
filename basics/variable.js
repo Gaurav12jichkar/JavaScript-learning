f2d9e62368a0bc67bcd1ab756f1c718524c3c8df
gaurav jichkar <127932525+Gaurav12jichkar@users.noreply.github.com>
2024-06-17 06:33:27 +0000

    secondday

---
+++ basics/variable.js
@@ -0,0 +1,6 @@
+let accountname= "gaurav jichkar";
+const accountId="12345";
+var  acountcity="nagpur";
+console.log(accountId);
+console.log(acountcity);
+console.table([accountname,accountId,acountcity])
