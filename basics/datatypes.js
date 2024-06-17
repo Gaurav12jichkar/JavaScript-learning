bb0fd8978460bf7923f55a5920ca82fabf052c86
gaurav jichkar <127932525+Gaurav12jichkar@users.noreply.github.com>
2024-06-17 06:54:57 +0000

    3rd day datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+console.log("heelo bhai hum yha pe datatypes padne vale hai")
+//primitve datatype
+//number=2 to the power53
+//string="g---"
+//boolean="true,false"
+//null=standard value
+//undefined =>value nodefined by the datatype
+console.log(typeof undefined)
+//.........................................................................
+
+//non primitive
+ //object
+ console.log(typeof null);//object
